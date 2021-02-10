f720714dcfb7fc51ff6675b2e3ab39e1d87cd1b0
EllieAzaveda <elliemiller66@icloud.com>
2021-02-09 21:01:54 -0700

    Add finished Unicorn and Dragon exercises

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,26 @@
+var hasMeal = 0;
 
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    if (hungry === undefined) {
+      this.hungry = true;
+    } else {
+      this.hungry = hungry;
+    }
+  }
+
+  greet(greeting){
+      return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    hasMeal++;
+    if(hasMeal >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
