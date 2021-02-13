f39d42244f6cf8efd6d08091458c669b9d28c032
EllieAzaveda <elliemiller66@icloud.com>
2021-02-12 22:24:59 -0700

    Add finished Magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,15 +1,16 @@
 class Magician {
-  constructor({ name, assistant, clothing } = {}, favoriteAccessory, spell, charm) {
+  constructor({ name, assistant, clothing } = {}, favoriteAccessory, spell, charm, confidencePercentage) {
     this.name = `The Great ${name}`;
     this.assistant = assistant;
-    this.favoriteAccessory = favoriteAccessory;
-    if(favoriteAccessory === undefined){
+    if (clothing === undefined) {
       this.favoriteAccessory = 'top hat';
     } else {
       this.favoriteAccessory = clothing;
     }
+    this.clothing = clothing;
     this.spell = spell;
     this.charm = charm;
+    this.confidencePercentage = 10;
   }
 
 performIncantation(x){
@@ -20,11 +21,21 @@ performIncantation(x){
   }
 };
 
-performTrick( {clothing} = {}) {
-  if(this.clothing === 'top hat'){
+performTrick(clothing) {
+  this.confidencePercentage += 10;
+  if(this.clothing === 'top hat' || this.favoriteAccessory === 'top hat'){
     return 'PULL RABBIT FROM TOP HAT';
   } else {
     return 'PULL DOVE FROM SLEEVE';
+  }
+}
+
+// Come back to this and figure out how to stop confidence at 100%
+performShowStopper() {
+  if (this.confidencePercentage >= 100 && this.assistant === true) {
+    return 'WOW! The magician totally just sawed that person in half!';
+  } else {
+    return 'Oh no, this trick is not ready!'
   }
 }
 
