a64408cd1e42bd3741b804ab17934cefb1c0032f
EllieAzaveda <elliemiller66@icloud.com>
2021-02-17 15:58:55 -0700

    Refactor hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -2,13 +2,13 @@ class Hobbit {
   constructor({ name } = {}, age, adult, old, hasRing) {
     this.name = name;
     this.adult = adult;
-    this.hasRing = hasRing;
+    this.hasRing = false;
     this.age = age || 0;
     this.old = false;
   }
 
   celebrateBirthday() {
-    this.age += 1;
+    this.age ++;
     if(this.age <= 32) {
       this.adult = false;
     } else if (this.age >= 101) {
@@ -24,7 +24,6 @@ class Hobbit {
       this.hasRing = true;
       return 'Here is the ring!';
     } else {
-      this.hasRing = false;
       return 'You can\'t have it!';
     }
   }
