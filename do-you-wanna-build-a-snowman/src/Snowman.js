7a1df7e8267cd76fc83152ddbaba14778c7db636
EllieAzaveda <elliemiller66@icloud.com>
2021-02-16 17:38:23 -0700

    Add completed snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,4 +1,17 @@
 class Snowman {
+  constructor(snowmanDetails, magicHat = false) {
+    this.carrots = snowmanDetails.carrots;
+    this.coal = snowmanDetails.coal;
+    this.buttons = snowmanDetails.buttons;
+    this.snowballs = snowmanDetails.snowballs;
+    this.magicHat = magicHat;
+  }
+
+  canWearMagicHat() {
+    if (this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+      this.magicHat = true;
+    }
+  }
 
 }
 
