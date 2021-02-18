2ef5c063fe2482c10b6186a82071d76e77e76b27
EllieAzaveda <elliemiller66@icloud.com>
2021-02-17 22:51:25 -0700

    Add completed Medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    var newStatue = new Statue(victim.name);
+    if (this.statues.length < 3) {
+      this.statues.push(newStatue);
+    } else {
+      var freeVictims = this.statues.shift();
+      freeVictims.mood = "relieved";
+      this.statues.push(newStatue);
+      var person = new Person(freeVictims.name, freeVictims.mood);
+      return person;
+    }
+  }
+
+};
+
+module.exports = Medusa;
